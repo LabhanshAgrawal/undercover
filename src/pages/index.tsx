a7e5b636fac0d97d0b21c8f25587967831446d81
Labhansh Agrawal <labhansh.agrawal@gmail.com>
2023-05-10 17:30:49 +0530

    black bg

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -34,7 +34,7 @@ export default function Home() {
 
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+      className={`flex min-h-screen flex-col items-center justify-between p-24 bg-black ${inter.className}`}
     >
       {!gameStarted ? <Players
         players={players}
